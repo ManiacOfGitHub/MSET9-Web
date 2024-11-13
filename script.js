8a964f6603e52443e2b86dbe03c2d0b85944a724
ManiacOfGitHub <43019219+ManiacOfGitHub@users.noreply.github.com>
2024-11-12 23:20:40 -0500

    Second commit

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ var m9status = {
     supportCode: "MWS00"
 };
 $(document).ready(async()=>{
+    if(!window["showDirectoryPicker"]) {
+        $("#welcomeStatus").text("Your browser is incompatible with this web application.");
+        $("#return").show();
+        m9status.supportCode = "MWE01";
+        return;
+    }
+
     setInterval(function() {
         if(navigator.onLine) {
             m9status.connected = true;
@@ -20,17 +27,48 @@ $(document).ready(async()=>{
         } else {
             m9status.sdInserted = false;
             $("#sdStatusIcon").text("pending");
-            $("#sdStatusText").text("SD Card not inserted");
+            $("#sdStatusText").text("Waiting for SD Card selection");
         }
+        $("#supportIdText").text(m9status.supportCode);
         $("#status-bar").removeClass("opacity-0");
-    },100);
+    },250);
 
 
-    if(!window["showDirectoryPicker"]) {
-        $("#welcomeStatus").text("Your browser is incompatible with this web application.");
-        $("#return").show();
-        m9status.supportCode = "MWE01";
-        return;
-    }
-    $("#welcomeStatus").text("Insert your SD card into your device, then press Continue.");
+    $("#welcomeStatus").text("Power off your console and insert your SD card into this device, then press Continue.");
+    $("#showQuickNotes").removeClass("d-none");
+    var quickNotesModal = new bootstrap.Modal("#quickNotes", {backdrop:'static',keyboard:false});
+    $("#showQuickNotes").click(()=>{
+        quickNotesModal.show();
+        $("#welcome-section").hide();
+        $("#main-area").hide();
+    });
+    var quickNotesState = 0;
+    $("#quickNotesContinue").click(()=>{
+        if(quickNotesState == 0) {
+            quickNotesState = 1;
+            $("#quickNotesTitle").text("Get Help!");
+            $("#quickNotesBody").text("If you need assistance at any point during this guide, click on the support section as shown on the top right of this page.");
+            $("#supportSection").addClass("showSupport");
+        } else if(quickNotesState == 1) {
+            quickNotesState = 2;
+            $("#quickNotesTitle").text("MSET9-Web v0beta");
+            $("#quickNotesBody").text("This web application is still in early beta, so there are likely going to be issues. Again, I cannot stress this enough, make a backup of any important data before beginning.");
+            $("#supportSection").removeClass("showSupport");
+        } else if(quickNotesState == 2) {
+            quickNotesModal.hide();
+            selectSdPrompt();
+        }
+    })
 });
+
+function selectSdPrompt() {
+    $("#select-sd-section").show();
+    $("#main-area").show();
+    $("#video-tutorial-source").attr("src","assets/tutorials/select-sd-windows.mp4");
+    $("#video-tutorial-section").show();
+    $("#video-tutorial-source")[0].parentElement.load();
+    $("#secondary-area").show();
+    $("#select-sd-button").click(()=>{
+        alert("not implemented\n\np.s. bonk gabbi lmao");
+    });
+}
